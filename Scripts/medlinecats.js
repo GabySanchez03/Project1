0862959717f89db37aa8c3971b277a6af1a5e181
GabySanchez03 <gmiranda@icstars.org>
2021-11-22 19:59:05 -0600

    first commit

---
+++ Scripts/medlinecats.js
@@ -0,0 +1,23 @@
+var myHeading=document.querySelector('h1');
+
+myHeading.textContent='Hello '+'Medline!'; //you can do 5+11 and it will add it is you dont have quotes
+var iceCream='vanilla';
+if(iceCream=== 'chocolate'){
+    alert('Hooray, I love '+iceCream+ ' ice cream!')
+}
+if(iceCream=== 'vanilla'){
+    alert('yummy yummy for my tummy')
+}
+else {
+    alert('awww, but '+iceCream+' is my favorite...')
+    };
+
+var myCats='Samson'; /* This is a string. A string is just a sequence of words*/
+var myCats='10'; /*Number example*/
+var myCats=true; /*boolean example - true or false */
+var myCats=[Samson,'10', Cats,true] /*Array example - many different values all in one reference*/
+var myHeading=document.querySelector('h1'); /*Object example - all above can be considered objects*/
+// another way to comment is two slashes hehe!!
+//and it can go to another space and not have to end the comment cause the browser already knows!!!
+
+//an operator is a mathematical symbol
